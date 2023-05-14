3d6a0c4a5cde7aa24043a5684ee8ea68ff454ca9
unknown <balloigor66@gmail.com>
2023-05-14 10:06:56 +0000

    last push

---
+++ resources/views/courses.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Sam Building</title>
+    <meta name="description" content="My Application Description">
+    <meta name="author" content="Sammy">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" href="img/gallery-7.PNG">
+
+    <!-- Daisy Ui Css link -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.14.1/dist/full.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Alpine Js Link -->
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js"
+        integrity="sha512-7x3zila4t2qNycrtZ31HO0NnJr8kg2VI67YLoRSyi9hGhRN66FHYWr7Axa9Y1J9tGYHVBPqIjSE1ogHrJTz51g=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+    ></script>
+    <script
+        defer
+        src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"
+    ></script>
+
+</head>
+<body class="font-body">
+    <div class="bg-[#1da1f2]">Christa</div>
+</body>
+</html>
